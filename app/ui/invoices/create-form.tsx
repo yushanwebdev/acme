4f6fb125e17b68880992211cc6c9405d5d47036d
Yushan Fernando <yushanwebdev@gmail.com>
2024-01-14 16:19:07 +0530

    feat: Imporoving Accessibility & Error handling

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -8,10 +10,17 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/action';
+import { useFormState } from 'react-dom';
+import { useHydrated } from '@/app/lib/hooks';
+import ErrorList from '../error-list';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const isHydrated = useHydrated();
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
+
   return (
-    <form action={createInvoice}>
+    <form id="invoice-create-form" action={dispatch} noValidate={isHydrated}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -24,6 +33,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
+              required
             >
               <option value="" disabled>
                 Select a customer
@@ -36,6 +47,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          <ErrorList errors={state.errors?.customerId} id="customer-error" />
         </div>
 
         {/* Invoice Amount */}
@@ -52,10 +64,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
+                required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          <ErrorList errors={state.errors?.amount} id="amount-error" />
         </div>
 
         {/* Invoice Status */}
@@ -72,6 +87,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="pending"
@@ -97,6 +113,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
+          <ErrorList errors={state.errors?.status} id="status-error" />
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
